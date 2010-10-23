6df2814641be5d4a53f9e4dc08a539aaddbacea4
James Wilson <jwilson@lithiumcorp.com>
2010-10-23 16:09:43 +1100

    5.0.9 near final, rrdtool 1.4 build

--- induction/src/build_config.h
+++
@@ -1,6 +0,0 @@
-/* src/build_config.h.  Generated from build_config.h.in by configure.  */
-/* Define socklen_t to an int if it isnt already defined */
-#define socklen_t int
-
-/* OS Type */
-#define OS_DARWIN 1
--- induction/src/version.h
+++
@@ -1,6 +0,0 @@
-/* src/version.h.  Generated from version.h.in by configure.  */
-/* Version */
-#define INDUCTION_VERSION "5.0.9"
-
-/* version.c */
-char* i_version ();
