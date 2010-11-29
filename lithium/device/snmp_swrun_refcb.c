493f196c7388a9e75f790e7e637034b3d05d873d
James Wilson <jwilson@lithiumcorp.com>
2010-11-29 16:17:31 +1100

    fixed procpro initial refresh bug

--- lithium/device/snmp_swrun_refcb.c
+++ lithium/device/snmp_swrun_refcb.c
@@ -40,11 +40,6 @@ int l_snmp_swrun_cnt_refcb (i_resource *self, i_entity *ent, void *passdata)
   i_container *procpro_cnt = l_procpro_cnt ();
   if (procpro_cnt) 
   {
-    /* DEBUG */
-    i_printf(0, "l_snmp_swrun_cnt_refcb about to refresh procpro container with swrun container showing refresh result %i and child count of %i", 
-      ent->refresh_result, ent->child_list ? ent->child_list->size : 0);
-    /* END DEBUG */
-
     i_entity_refresh (self, ENTITY(procpro_cnt), REFFLAG_AUTO, NULL, NULL);
   }
   
