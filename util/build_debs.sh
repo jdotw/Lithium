33b33052edb6661257d56cde36b24a5444497c99
James Wilson <jwilson@ubuntu-build.localdomain>
2010-12-14 16:01:23 +1100

    fixed deb build script

--- util/build_debs.sh
+++ util/build_debs.sh
@@ -8,6 +8,8 @@ debuild clean
 debuild binary
 cd ..
 
-scp libinduction5_*deb root@vmstudio:/opt/vmware/www/ISV/libinduction5
-scp libinduction5-dev_*deb root@vmstudio:/opt/vmware/www/ISV/libinduction5-dev
-scp lithiumcore_*deb root@vmstudio:/opt/vmware/www/ISV/lithiumcore
+scp libinduction5_*deb root@10.15.1.17:/opt/vmware/www/ISV/libinduction5
+scp libinduction5-dev_*deb root@10.15.1.17:/opt/vmware/www/ISV/libinduction5-dev
+scp lithiumcore_*deb root@10.15.1.17:/opt/vmware/www/ISV/lithiumcore
+
+rm *deb
