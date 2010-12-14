c8ed64c67dd5c015cadfe7b126163fb3dff08fde
James Wilson <jwilson@ubuntu-build.localdomain>
2010-12-14 12:53:51 +1100

    deb update

--- util/build_debs.sh
+++ util/build_debs.sh
@@ -7,3 +7,7 @@ cd core
 debuild clean
 debuild binary
 cd ..
+
+scp libinduction5_*deb root@vmstudio:/opt/vmware/www/ISV/libinduction5
+scp libinduction5-dev_*deb root@vmstudio:/opt/vmware/www/ISV/libinduction5-dev
+scp lithiumcore_*deb root@vmstudio:/opt/vmware/www/ISV/lithiumcore
