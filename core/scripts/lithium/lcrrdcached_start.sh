005337d0be016ad5131bd181abc82e65bab48cdd
James Wilson <jwilson@lithiumcorp.com>
2010-12-15 13:50:26 +1100

    updated lcrrdcached_start script

--- core/scripts/lithium/lcrrdcached_start.sh
+++ core/scripts/lithium/lcrrdcached_start.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 killall -9 lcrrdcached
 rm /var/tmp/.lcrrdcached.pid
 rm /var/tmp/.lcrrdcached.sock
 
-/Library/Lithium/LithiumCore.app/Contents/MacOS/lcrrdcached -g -l /var/tmp/.lcrrdcached.sock -p /var/tmp/.lcrrdcached.pid
+if [ -f "/Library/Lithium/LithiumCore.app/Contents/MacOS/lcrrdcached" ] ; then
+  CACHEDPATH="/Library/Lithium/LithiumCore.app/Contents/MacOS/lcrrdcached"
+else
+  CACHEDPATH="/usr/bin/lcrrdcached"
+fi
+
+$CACHEDPATH -g -l /var/tmp/.lcrrdcached.sock -p /var/tmp/.lcrrdcached.pid
 
